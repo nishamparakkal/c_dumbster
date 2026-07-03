59a0e4511b7daf0192f53853ce935f03def5d717
nisham <mohammednishamp@gmail.com>
2026-07-03 23:43:44 +0530

    initial commit

---
+++ number_is_negative_or_not.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+
+{
+
+int num;
+
+printf("enter the number:");
+scanf("%d",&num);
+if(num<0)
+printf("number is negative");
+else if(num==0) 
+printf("number is zero");
+else
+printf("number is positive");
+return 0;
+
+
+
+}
